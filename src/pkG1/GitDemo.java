64ade26ddac510d7387203299c02e12553b81b1c
Ankit <ankitgond001@gmail.com>
2021-08-17 11:59:55 +0530

    New Line added.

--- src/pkG1/GitDemo.java
+++ src/pkG1/GitDemo.java
@@ -7,6 +7,7 @@ public class GitDemo {
 		System.out.println("Hello from Ankit");
 		System.out.println("New Commit From ankit");
 		System.out.println("This is master branch");
+		System.out.println("New Line added.");
 	}
 
 }
